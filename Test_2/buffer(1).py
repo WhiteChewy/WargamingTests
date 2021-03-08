dade2abecef02c4065d1f7d301a844b2f90afd8a
Nikita <pocketkurt@gmail.com>
2021-03-08 22:04:59 +0300

    Remove inherit from deque

--- Test_2/buffer(1).py
+++ Test_2/buffer(1).py
@@ -1,41 +1,55 @@
+# -*- coding: utf-8 -*-
 """
 Реализация циклического буфера FIFO (first in first out) через collections.deque
 
 (c) 2021 Куликов Никита, Санкт-Петербург, Россия
 e-mail: pocketkurt@gmail.com
 """
+
 from collections import deque
 
 
-class CycledBuffer(deque):
+class CycledBuffer:
 
     def __init__(self, size=5):
-        super(CycledBuffer, self).__init__(maxlen=size)
-        self.size = self.maxlen
-        self.slots = self.size
+        self.buf = deque(maxlen=size)
+        self.slots = self.buf.maxlen
 
     def __str__(self):
-        res = "["
-        for i in range(len(self)):
-            if i == 0:
-                res += str(self[i])
-            else:
-                res += ", " + str(self[i])
-        res += "]"
-        return res
+        return str(list(self.buf))
 
     def append(self, value):
-        super(CycledBuffer, self).append(value)
+        self.buf.append(value)
         if self.slots != 0:
             self.slots -= 1
 
     def isEmpty(self):
-        return len(self) == 0
+        return not bool(self.buf)
 
     def clear(self):
-        super(CycledBuffer, self).clear()
-        self.slots = self.size
+        self.buf.clear()
+        self.slots = self.buf.maxlen
 
     def pop(self):
         self.slots += 1
-        return super(CycledBuffer, self).popleft()
+        return self.buf.popleft()
+
+
+x = CycledBuffer()
+x.append(1)
+print(x)
+x.append(2)
+print(x)
+x.append(3)
+print(x)
+x.append(4)
+print(x)
+x.append(5)
+print(x)
+x.append(6)
+print(x)
+print(x.pop())
+print(x.isEmpty())
+print(x)
+print(x.clear())
+print(x)
